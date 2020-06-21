b9ae00b6de5c6c5f27044bd96f7e5bfea081795d
Tomithy83 <tomharner83@gmail.com>
2020-06-21 13:35:53 -0500

    class-ified

--- space invaders.py
+++ space invaders.py
@@ -18,7 +18,17 @@ scrn = turtle.Screen()
 scrn.bgcolor(bg)
 scrn.title(title)
 
-	
+#create the debug pen
+debugpen = turtle.Turtle()
+debugpen.speed(0)
+debugpen.color(fg)
+debugpen.hideturtle()
+debugpen.up()
+#define the debug messaging method
+def drwmsg(msg): 
+	debugpen.clear()
+	debugpen.setposition(-width/2+10, height/2-40)
+	debugpen.write(msg)
 
 #create the border
 def draw_border(color):
@@ -38,35 +48,43 @@ def draw_border(color):
 			border_pen.forward(height)
 		border_pen.lt(90)
 
+class player():
+	playerspeed = 15
+	
+	def __init__(self, color):
+		self.t = turtle.Turtle()
+		self.t.speed(0)
+		self.t.up()
+		self.t.color(color)
+		self.t.sety(-height*buffer)
+		self.t.lt(90)
+		self.testmsg = 'test'
 
-player = turtle.Turtle()
-player.speed(0)
-player.up()
-player.color(fg)
-player.sety(-height*buffer)
-player.lt(90)
-
-#move player left
-def moveleft():
-	player.setx(player.xcor()-playerspeed)
-	if player.xcor() < (-width * buffer):
-		player.setx(-width * buffer)
-
-#move player right
-def moveright():
-	player.setx(player.xcor()+ playerspeed)
-	if player.xcor() > (width * buffer):
-		player.setx(width * buffer)
+	#move player left
+	def moveleft(self):
+#		drwmsg('move left ' + str(self.t.xcor()))
+		self.t.setx(self.t.xcor()-player.playerspeed)
+		if self.t.xcor() < (-width * buffer):
+			self.t.setx(-width * buffer)
+	
+	#move player right
+	def moveright(self):
+#		drwmsg('move right' + str(self.t.xcor()))
+		self.t.setx(self.t.xcor()+ player.playerspeed)
+		if self.t.xcor() > (width * buffer):
+			self.t.setx(width * buffer)
 
 def getclickcoor(x,y):
-#	player.goto(x,y)
-#	moveright()
+	drwmsg('the click registered x: ' + str(x) + 'y: ' + str(y))
 	if x > 20:
-		moveright()
+		player1.moveright()
+#		drwmsg('>20')
 	if x < -20:
-		moveleft()
+		player1.moveleft()
+#		drwmsg('<-20')
 
-
+player1 = player(fg)
+#drwmsg(player1.__dict__)
 draw_border(fg)
 scrn.onclick(getclickcoor)
 scrn.listen()
